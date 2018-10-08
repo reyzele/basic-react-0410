7ce111aaa602ce2ee1db7451f6b76762f3d8e647
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-10-08 20:49:27 +0300

    add user form

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { findDOMNode } from 'react-dom'
 import ArticleList from './article-list'
 import ArticleChart from './articles-chart'
+import UserForm from './user-form'
 import articles from '../fixtures'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <UserForm />
         <ArticleList articles={articles} ref={this.setArticleListRef} />
         <ArticleChart articles={articles} />
       </div>
---
+++ src/components/user-form.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class UserForm extends Component {
+  state = {
+    username: ''
+  }
+
+  render() {
+    console.log('---', this.state.username)
+    return (
+      <div>
+        Username:{' '}
+        <input value={this.state.username} onChange={this.handleUserChange} />
+      </div>
+    )
+  }
+
+  handleUserChange = (ev) => {
+    if (ev.target.value.length > 10) return this.setState({ username: '' })
+
+    this.setState({
+      username: ev.target.value
+    })
+  }
+}
+
+export default UserForm
